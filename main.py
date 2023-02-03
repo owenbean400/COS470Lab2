b46264c1bc5216d9cec17fbe91672fbf69ea63b2
sad-panda-dev <80590323+sad-panda-dev@users.noreply.github.com>
2023-02-02 21:32:06 -0500

    Updated comments for main.py and words_surrounding_numbers.py

--- main.py
+++ main.py
@@ -33,6 +33,16 @@ def extract_temporal_expressions(file):
 
 
 def replaceMonthName(pattMatches):
+    # Function that takes in a list of temporal pattern matches
+    # returned from the above function. Iterates through the list
+    # looking for substring matches for Month string values. Given the
+    # format we need to normalize to, when a month name is matched
+    # to we take the numeric equivalent and append it to the
+    # beginning of the string.
+    #
+    # pattMatches : List -> list of temporal regex matches
+    # patternMatches : List -> resulting list of matches with month string
+    #                 value replaced by numeric value
 
     paternMatches = []
 
@@ -119,6 +129,21 @@ def replaceMonthName(pattMatches):
 
 
 def padWithZeros(data):
+    # Function to take into account that not all of our day values
+    # found within the documents are not of length 2. At this point
+    # we know that we no longer have character values within the
+    # date strings, so we check each item in the list -2 index. If we
+    # find a "/" we know that we have a day value with only one number.
+    # In that case we then append a 0 before the number following the
+    # "/" and append it to the result list. If we find a number instead
+    # of the "/" we just append the item to the result list.
+    #
+    # data : list -> list of either numeric date and month strings or
+    #         date and year strings
+    # paddedOutput : list -> result list where any string that had a
+    #         date value of length 1 has been padded with a zero,
+    #         otherwise the original list value is just appended to the
+    #         result list
 
     paddedOutput = []
     padded = ''
@@ -136,6 +161,20 @@ def padWithZeros(data):
 
 
 def normalize(data):
+    # Function to take a list of numeric date string values of
+    # either month/day or month/year. By checking the -3 index
+    # we can tell which type it is by whether it finds a digit or
+    # not. It will replace any "/" with " " and then use pythons
+    # split() method to create a sublist of the orginal list item.
+    # If it found a digit at the -3 index we append "/xx/" inbetween
+    # the two substrings. Otherwise we append "/xxxx" to the end of
+    # the original string. Then append the normalized string to the
+    # result list.
+    #
+    # data : list -> list of numeric date string values of either
+    #        month/day or month/year
+    # normalizedData : list -> result list of all returned strings of
+    #        the format 'dd/xx/dddd' or 'dd/dd/xxxx'
 
     normalizedData = []
     normal = ''
--- words_surrounding_numbers.py
+++ words_surrounding_numbers.py
@@ -6,10 +6,19 @@ from collections import Counter
 
 
 def find_words_around_numbers(file_path):
+    # Function that will open a .txt file and search for a pattern of a
+    # number followed by a word, or a word followed by a number. Using pythons
+    # re library it defines the patterns and searches the .txt file
+    # for matches. If a match is found then the word will be appended to the
+    # result list.
+    #
+    # file_path : String -> string representation of the file path to be searched
+    # results : list -> result list of words found within the pattern matches
+
     with open(file_path, 'r') as file:
         text = file.read()
 
-    # Regular expression pattern to match a words before and after a number
+    # Regular expression pattern to match all words before or after a number
     pattern1 = "\s*\w+ \d+"
     pattern2 = "\s*\d+ \w+"
 
@@ -30,6 +39,13 @@ def find_words_around_numbers(file_path):
 
 
 def find_words_before_numbers(file_path):
+    # Function that will open a .txt file and search for a pattern of
+    # a word followed by a number. Using pythons re library it defines
+    # the pattern and searches the .txt file for matches. If a match
+    # is found then the word will be appended to the result list.
+    #
+    # file_path : String -> string representation of the file path to be searched
+    # results : list -> result list of words found within the pattern matches
 
     with open(file_path, 'r') as file:
         text = file.read()
@@ -52,7 +68,15 @@ def find_words_before_numbers(file_path):
 
     return results
 
+
 def find_words_after_numbers(file_path):
+    # Function that will open a .txt file and search for a pattern of
+    # a number followed by a word. Using pythons re library it defines
+    # the pattern and searches the .txt file for matches. If a match
+    # is found then the word will be appended to the result list.
+    #
+    # file_path : String -> string representation of the file path to be searched
+    # results : list -> result list of words found within the pattern matches
     with open(file_path, 'r') as file:
         text = file.read()
 
@@ -76,6 +100,12 @@ def find_words_after_numbers(file_path):
 
 
 def plot_wordcloud(words):
+    # Function to generate a word cloud using pythons word cloud generator.
+    # It takes a dictionary of the top 20 found words and their frequencies.
+    # It then uses matplotlib library to plot the generated word cloud.
+    #
+    # words : dictionary -> dictionary of the 20 most frequent words and their
+    #         frequencies
     wordcloud = WordCloud(width=1200, height=800, background_color ='white',
                           min_font_size = 14).generate_from_frequencies(words)
     plt.figure(figsize=(30, 20))
@@ -85,6 +115,14 @@ def plot_wordcloud(words):
 
 
 def search_directory(directory):
+    # Function acts as a directory crawler to first locate the subdirectory
+    # labeled "input_dir" which is where the .txt files we were told to
+    # focus on are located. It then keeps searching until it finds .txt files.
+    # It acheives this using pythons os library. Once the file is found it is
+    # then passed through the find_words_around_numbers function.
+    #
+    # directory: file directory -> dircetory structure that holds all of the .txt
+    #            files(dataset)
     results = []
     for subdir, dirs, files in os.walk(directory):
         for sdir in dirs:
@@ -101,13 +139,19 @@ def search_directory(directory):
     return results
 
 
-
+# List to store list of lists that is returned by search_directory as single list
+# to pass to Counter
 word_list = []
+# The result of the search_directory function call
 list_of_words = search_directory("txt")
 
+# Iterate over the list of lists to convert into a single list of strings
 for sublist in list_of_words:
     word_list += sublist
 
+# Resulting dictionary of the top 20 most frequently found words and their frequencies
+# after passing the list of words to the Counter.most_common(20)
 top_20_words = dict(Counter(word_list).most_common(20))
 print(top_20_words)
+# Pass dictionary to the plot_wordcloud function to generate the word cloud
 plot_wordcloud(top_20_words)
